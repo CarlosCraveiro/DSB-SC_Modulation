4292b8d432cee9e51b2f963ca3b91571e952a2b6
CarlosCraveiro <carlos.craveiro@usp.br>
2024-05-23 16:52:24 -0300

    Adds nix files

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "DSB-SC Modulation in GNU/Octave";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./nix/shell.nix { inherit pkgs; };
+        }
+      );
+}
